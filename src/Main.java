af35781a68738e549068a06c4d4816623a5305f4
Bhawna Adhikari <bhawna.ad05@gmail.com>
2023-02-04 21:59:37 +0530

    add output

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
         itr.insert(4110523, new Data("John Doe", 23, "9981195618", new String[]{"Cpp", "Python", "JavaScript"}));
         itr.insert(4110524, new Data("John Doe", 23, "9981195617", new String[]{"Cpp", "Python", "JavaScript"}));
 
+        System.out.println();
+        System.out.println();
         itr.display();
         System.out.println();
         System.out.println(itr.hasNext());
